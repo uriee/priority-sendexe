9aa3701f7a35277bc196d025bc608868fb971baf
uriee <uevenezra@gmail.com>
2019-04-30 12:19:52 +0300

    added backup of a sample priority procedure that converts data to json

--- bin.js
+++ bin.js
@@ -1,4 +1,3 @@
-const send = require('.') /* the current working directory so that means main.js because of package.json */
-//let theFile = process.argv[2] /* what the user enters as first argument */
-console.log(send)
+const send = require('.') 
+//let theFile = process.argv[2] /* extract first argument */
 send()
